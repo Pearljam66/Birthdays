8f33eca42a3b897b83dd6c23c24f64263524616e
Sarah Clark <sarahireneclark@gmail.com>
2024-05-10 11:28:24 -0500

    Initial commit of project

---
+++ Birthdays/Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Sarah Clark on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Birthdays/Birthdays/Models/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Sarah Clark on 5/10/24.
+//
+
+import Foundation
+
+struct Friend {
+    let friendName: String
+    let birthdate: Date
+}
